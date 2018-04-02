8afb67bf2a2782a91c2c517f0d683617b3f04905
Felipe Ferreira <felipe.ferreira.ads@gmail.com>
2018-04-02 09:55:17 -0300

    First commit

---
+++ source/js/components/carousel.js
@@ -0,0 +1,32 @@
+import 'owl.carousel'
+
+var width = $(window).width()
+var stagePadding = 0
+if(width > 1050 && width <= 1150){
+    stagePadding = 50
+} else if(width <= 1050)  {
+    stagePadding = 120
+} else {
+    stagePadding = 100
+}
+
+$('.carousel').owlCarousel({
+    items: 3,
+    dots: false,
+    navigation : false,
+    //stagePadding: stagePadding,
+    responsive:{
+        320:{
+            items:1,
+        },
+        1050:{
+            items:2
+        },
+        1150:{
+            items:3
+        },
+        1550:{
+            items: 3
+        }
+    }
+})
