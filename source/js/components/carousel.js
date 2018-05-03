17de0ebc17b943063a71d5269284b3be1f46bfd1
Felipe Ferreira <felipe.ferreira.ads@gmail.com>
2018-05-02 22:49:47 -0300

    Add Pages

--- source/js/components/carousel.js
+++ source/js/components/carousel.js
@@ -1,25 +1,10 @@
 import 'owl.carousel'
 
-var width = $(window).width()
-var stagePadding = 0
-if(width > 1050 && width <= 1150){
-    stagePadding = 50
-} else if(width <= 1050)  {
-    stagePadding = 120
-} else {
-    stagePadding = 100
-}
-
 $('.treatments .carousel').owlCarousel({
-    items: 3,
     navigation : false,
-    //stagePadding: stagePadding,
     responsive:{
         320:{
             items:1,
-        },
-        1050:{
-            items:2
         },
         1150:{
             items:2
